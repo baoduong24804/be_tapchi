d33c9f02c148b8a8afdcea3f2802fe45ae17a669
hisu87 <numpadagain@gmail.com>
2024-10-16 14:15:58 +0700

    :+1:
    OKeal

--- pjtapchi/src/main/java/com/be/tapchi/pjtapchi/api/TestAPI.java
+++ pjtapchi/src/main/java/com/be/tapchi/pjtapchi/api/TestAPI.java
@@ -1,15 +1,10 @@
 package com.be.tapchi.pjtapchi.api;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.be.tapchi.pjtapchi.model.Taikhoan;
+import com.be.tapchi.pjtapchi.service.BangGiaQCService;
 import com.be.tapchi.pjtapchi.service.EmailService;
+import com.be.tapchi.pjtapchi.service.HoaDonService;
 import com.be.tapchi.pjtapchi.service.TaiKhoanService;
-
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,26 +12,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.be.tapchi.pjtapchi.model.BangGiaQC;
-import com.be.tapchi.pjtapchi.model.HoaDon;
-import com.be.tapchi.pjtapchi.model.HopDong;
-import com.be.tapchi.pjtapchi.model.Taikhoan;
-import com.be.tapchi.pjtapchi.service.BangGiaQCService;
-import com.be.tapchi.pjtapchi.service.HoaDonService;
-import com.be.tapchi.pjtapchi.service.TaiKhoanService;
-
-
+import java.util.List;
 
 
 @RestController
 @RequestMapping("api")
 public class TestAPI {
     @Autowired
+    EmailService emailService;
+    @Autowired
     private TaiKhoanService taiKhoanService;
-
     @Autowired
     private BangGiaQCService bangGiaQCService;
-
     @Autowired
     private HoaDonService hoaDonService;
 
@@ -46,7 +33,7 @@ public class TestAPI {
         ApiResponse<List<Taikhoan>> response = new ApiResponse<>(true, "Fetch successful", list);
 
         if (list.isEmpty()) {
-            
+
             return ResponseEntity.ok().body(response);
         } else {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
@@ -54,14 +41,11 @@ public class TestAPI {
 
     }
 
-    @Autowired
-    EmailService emailService;
-
     @GetMapping("email")
     public String getMethodName() {
-        emailService.sendActivationEmail("tvugiang@gmail.com", "aaa");
+        emailService.sendActivationEmail("numpadagain@gmail.com", "ádasdasdasd");
         return "Thanh cong";
     }
-    
+
 
 }
