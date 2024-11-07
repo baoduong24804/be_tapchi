2920ed660f33031944e1263cdb08f4f145d417ca
Zitjjj <sora68507@gmail.com>
2024-11-07 22:36:03 +0700

    tet

--- pjtapchi/src/main/java/com/be/tapchi/pjtapchi/service/DanhMucService.java
+++ pjtapchi/src/main/java/com/be/tapchi/pjtapchi/service/DanhMucService.java
@@ -1,8 +1,19 @@
 package com.be.tapchi.pjtapchi.service;
 
+import com.be.tapchi.pjtapchi.model.DanhMuc;
+import com.be.tapchi.pjtapchi.repository.DanhMucRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class DanhMucService {
+    @Autowired
+    private static DanhMucRepository danhMucRepository;
+
+    public static List<DanhMuc> getAllDanhMuc() {
+        return danhMucRepository.findAll();
+    }
+
 }
