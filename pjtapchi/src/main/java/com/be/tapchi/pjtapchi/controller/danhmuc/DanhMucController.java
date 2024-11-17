ebd9ce629620453da06842399e5c056cf8f3f215
Zitjjj <sora68507@gmail.com>
2024-11-17 23:17:38 +0700

    update DanhMucController

--- pjtapchi/src/main/java/com/be/tapchi/pjtapchi/controller/danhmuc/DanhMucController.java
+++ pjtapchi/src/main/java/com/be/tapchi/pjtapchi/controller/danhmuc/DanhMucController.java
@@ -1,18 +1,13 @@
 package com.be.tapchi.pjtapchi.controller.danhmuc;
 
 
-
-import com.be.tapchi.pjtapchi.model.Baibao;
-
 import com.be.tapchi.pjtapchi.controller.apiResponse.ApiResponse;
-import com.be.tapchi.pjtapchi.controller.danhmuc.model.Danhmucweek;
-
+import com.be.tapchi.pjtapchi.model.Baibao;
 import com.be.tapchi.pjtapchi.model.DanhMuc;
+import com.be.tapchi.pjtapchi.model.Danhmucbaibao;
+import com.be.tapchi.pjtapchi.repository.DanhMucBaiBaoRepository;
+import com.be.tapchi.pjtapchi.repository.DanhMucRepository;
 import com.be.tapchi.pjtapchi.service.DanhMucService;
-
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -21,8 +16,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
-import java.util.List;
+import java.util.*;
 
 
 @CrossOrigin(origins = "*")
@@ -34,30 +28,36 @@ public class DanhMucController {
     @Autowired
     DanhMucService danhMucService;
 
+    @Autowired
+    DanhMucBaiBaoRepository danhMucBaiBaoRepository;
+
+    @Autowired
+    DanhMucRepository danhMucRepository;
+
     @PostMapping("/get/week")
     public ResponseEntity<ApiResponse<?>> getDanhmucInCurrentWeek(
-            @RequestParam(defaultValue = "0") int page, 
+            @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size) {
         // TODO: process POST request
-            try {
-                ApiResponse<?> api = new ApiResponse<>();
-                api.setSuccess(true);
-                api.setMessage("Thành công lấy dữ liệu danh mục theo tuần");
-                Map<String,Object> map = new HashMap<>();
-                Page<DanhMuc> dm = danhMucService.getDanhmucInCurrentWeek(page, size);
-                map.put("data", dm.getContent());
-                map.put("totalPage", String.valueOf(dm.getTotalPages()));
-                map.put("pageNumber", String.valueOf(dm.getNumber()));
-                map.put("pageSize", String.valueOf(dm.getSize()));
-                map.put("totalElements", String.valueOf(dm.getTotalElements()));
-                api.setData(map);
-                return ResponseEntity.ok().body(api);
-            } catch (Exception e) {
-                // TODO: handle exception
-            }
-        
+        try {
+            ApiResponse<?> api = new ApiResponse<>();
+            api.setSuccess(true);
+            api.setMessage("Thành công lấy dữ liệu danh mục theo tuần");
+            Map<String, Object> map = new HashMap<>();
+            Page<DanhMuc> dm = danhMucService.getDanhmucInCurrentWeek(page, size);
+            map.put("data", dm.getContent());
+            map.put("totalPage", String.valueOf(dm.getTotalPages()));
+            map.put("pageNumber", String.valueOf(dm.getNumber()));
+            map.put("pageSize", String.valueOf(dm.getSize()));
+            map.put("totalElements", String.valueOf(dm.getTotalElements()));
+            api.setData(map);
+            return ResponseEntity.ok().body(api);
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
 
-            return ResponseEntity.badRequest().body(null);
+
+        return ResponseEntity.badRequest().body(null);
     }
 
     @GetMapping("/all")
@@ -76,28 +76,45 @@ public class DanhMucController {
     }
 
     @PostMapping("/create")
-    public ResponseEntity<ApiResponse<DanhMuc>> createBaibao(@RequestBody DanhMuc danhMuc) {
+    public ResponseEntity<ApiResponse<DanhMuc>> createDanhMuc(@RequestBody DanhMuc danhMuc) {
+        if (danhMuc.getStatus() == null) {
+            danhMuc.setStatus(0);
+        }
+        if (danhMuc.getNgayTao() == null) {
+            danhMuc.setNgayTao(new Date());
+        }
         DanhMuc dm = danhMucService.saveDanhMuc(danhMuc);
         ApiResponse<DanhMuc> response = new ApiResponse<>(true, "Create danh muc successful", dm);
         return ResponseEntity.ok().body(response);
     }
 
+
     @GetMapping("/delete/{id}")
-    public ResponseEntity<ApiResponse<Void>> deleteDanhMuc(@PathVariable("id") Long id) {
+    public ResponseEntity<ApiResponse<String>> deleteDanhMuc(@PathVariable("id") Long id) {
+        DanhMuc danhMuc = danhMucService.getDanhMucById(id);
+        if (danhMuc == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(new ApiResponse<>(false, "Danh mục không tồn tại", null));
+        }
         danhMucService.deleteDanhMuc(id);
-        return ResponseEntity.ok().body(new ApiResponse<>(true, "Delete danh muc successful", null));
+        return ResponseEntity.ok().body(new ApiResponse<>(true, "Delete danh muc successful", "Đã xóa danh mục với id = " + id));
     }
 
     @GetMapping("/findbyID/{id}")
     public ResponseEntity<ApiResponse<DanhMuc>> findById(@PathVariable("id") Long id) {
-        danhMucService.getDanhMucById(id);
-        return ResponseEntity.ok().body(new ApiResponse<>(true, "Find danh muc by ID successful", null));
-
+        DanhMuc dm = danhMucService.getDanhMucById(id);
+        if (dm == null) {
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .body(new ApiResponse<>(false, "Danh mục không tồn tại", null));
+        }
+        return ResponseEntity.ok().body(new ApiResponse<>(true, "Find danh muc by ID successful", dm));
     }
+
 
     @PostMapping("/update/{id}")
     public ResponseEntity<ApiResponse<DanhMuc>> updateDanhMuc(@PathVariable("id") Long id,
-            @RequestBody DanhMuc newDanhMuc) {
+                                                              @RequestBody DanhMuc newDanhMuc) {
         DanhMuc updatedDanhMuc = danhMucService.updateDanhMuc(id, newDanhMuc);
         if (updatedDanhMuc != null) {
             return ResponseEntity.ok().body(new ApiResponse<>(true, "Update danh muc successful", updatedDanhMuc));
@@ -107,4 +124,64 @@ public class DanhMucController {
         }
     }
 
+    // tìm baibao where danhmucid = {danhmucid}
+    @GetMapping("/{danhmucId}/listBb")
+    public ResponseEntity<ApiResponse<?>> getBaibaosByDanhMuc(@PathVariable Long danhmucId) {
+        try {
+            DanhMuc dmBaibao = danhMucRepository.findById(danhmucId).orElse(null);
+            System.out.println("1");
+            List<Danhmucbaibao> listDm = dmBaibao.getDanhmucbaibaos();
+            System.out.println("2");
+            List<Baibao> baibaos = new ArrayList<>();
+            System.out.println("3");
+            for (Danhmucbaibao danhmucbaibao : listDm) {
+                try {
+                    if (danhmucbaibao.getBaibao().getTaikhoan() == null) {
+                        continue;
+                    }
+                } catch (Exception e) {
+                    continue;
+                }
+                baibaos.add(danhmucbaibao.getBaibao());
+
+            }
+            System.out.println("4");
+            Map<String, Object> map = new HashMap<>();
+            List<Baibao> baibaoList = new ArrayList<>();
+            for (Baibao baibao : baibaos) {
+                try {
+                    if (baibao.getTaikhoan() == null) {
+                        continue;
+                    }
+                    if (baibao.getBinhluans() == null) {
+                        continue;
+                    }
+                    if (baibao.getThichs() == null) {
+                        continue;
+                    }
+                } catch (Exception e) {
+                    continue;
+                }
+                Baibao bb = new Baibao();
+                bb.setId(baibao.getId());
+                bb.setStatus(baibao.getStatus());
+                bb.setTaikhoan(baibao.getTaikhoan());
+                bb.setNoidung(baibao.getNoidung());
+                bb.setNgaydang(baibao.getNgaydang());
+                bb.setTieude(baibao.getTieude());
+                bb.setUrl(baibao.getUrl());
+                baibaoList.add(bb);
+            }
+            map.put("baibaoList", baibaoList);
+            if (baibaos.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                        .body(new ApiResponse<>(false, "Không có bài báo nào trong danh mục này", null));
+            }
+            return ResponseEntity.ok()
+                    .body(new ApiResponse<>(true, "Danh sach bai bao ", map));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
 }
--- pjtapchi/src/main/java/com/be/tapchi/pjtapchi/repository/DanhMucBaiBaoRepository.java
+++ pjtapchi/src/main/java/com/be/tapchi/pjtapchi/repository/DanhMucBaiBaoRepository.java
@@ -4,9 +4,9 @@ import com.be.tapchi.pjtapchi.model.Danhmucbaibao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface DanhMucBaiBaoRepository extends JpaRepository<Danhmucbaibao, Long> {
-    //List<Danhmucbaibao> findByDanhMucId(Long danhmucId);
+    Optional<Danhmucbaibao> findById(Long aLong);
 }
